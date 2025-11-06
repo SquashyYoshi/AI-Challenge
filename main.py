12b077597cad2c4e3c227aa3793e0c6d26b8a64b
SquashyYoshi <joshua.smith722@gmail.com>
2025-11-06 14:33:58 -0600

    11/6/25 work

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import sklearn
-
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import csv
+import numpy
 
 
 def main():
-    return
+    dataList = csvToDict("mini-AAPL.csv")
 
+    date = []
+    opend = []
+    for row in dataList:
+        opend.append(row["Open"])
+        date.append(row["Date"])
+
+    X = [opend]
+    y = date
+
+    X_train, X_test, y_train, y_test = sklearn.train_test_split(X, y, random_state=42, test_size = 0.20)
+    # slrModel = sklearn.LinearRegression() #creates an empty model
+    # slrModel.fit(X_train, y_train) #create the model
+
+    model = sklearn.LinearRegression()
+    model.fit(X_train, y_train)
+
+    
+
+def csvToDict(filename):
+    dataList = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            dataList.append(row)
+    return dataList
 
 
 
